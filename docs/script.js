b45bb92053d0d8f399f939db344ab9a865824506
lugomez92 <165215470+lugomez92@users.noreply.github.com>
2024-04-19 08:07:15 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,6 @@
 //Go to top Button
 let goTopBtn = document.getElementById("goTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,7 +11,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function goToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
